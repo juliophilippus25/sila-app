7643d7680b6a10758aa55e20b93f77c6aabaa13a
Julio Philippus <juliogerald25@gmail.com>
2025-04-06 23:23:36 +0700

    FEAT: add admin seeder

---
+++ database/seeders/AdminTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Carbon\Carbon;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userId = strtoupper(md5("!@#!@#" . Carbon::now()->format('YmdH:i:s')));
+
+        User::insert([
+            [
+              'id'              => $userId,
+              'name'            => 'Riswan',
+              'email'           => 'riswan@gmail.com',
+              'role'            => 'petugas',
+              'nip'             => '1234567891011123',
+              'password'        => bcrypt('password'),
+              'created_at'      => Carbon::now(),
+              'updated_at'      => Carbon::now()
+            ]
+        ]);
+    }
+}
