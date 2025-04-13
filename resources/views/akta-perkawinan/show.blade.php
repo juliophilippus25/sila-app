aa8a74ea992528a9aac0c9924712b03494db3060
Julio Philippus <juliogerald25@gmail.com>
2025-04-13 14:36:59 +0700

    FEAT: add data ayah istri

--- resources/views/akta-perkawinan/show.blade.php
+++ resources/views/akta-perkawinan/show.blade.php
@@ -44,6 +44,12 @@
                                 class="whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm">
                                 Data Istri
                             </button>
+                            <button @click="tab = 'ayah_istri'"
+                                :class="tab === 'ayah_istri' ? 'border-indigo-500 text-indigo-600' :
+                                    'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'"
+                                class="whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm">
+                                Data Ayah Istri
+                            </button>
                         </nav>
                     </div>
 
@@ -61,6 +67,9 @@
                             @include('akta-perkawinan.tabs.data-ibu-suami')
                         </div>
                         <div x-show="tab === 'istri'">
+                            @include('akta-perkawinan.tabs.data-istri')
+                        </div>
+                        <div x-show="tab === 'ayah_istri'">
                             @include('akta-perkawinan.tabs.data-istri')
                         </div>
                     </div>
---
+++ resources/views/akta-perkawinan/tabs/data-ayah-istri.blade.php
@@ -0,0 +1,44 @@
+<div class="space-y-2">
+    <div class="flex">
+        <span class="w-64 font-semibold text-gray-700">NIK</span>
+        <span class="text-gray-900">: {{ $aktaPerkawinan->perkawinanAyahIstri->nik ?? '-' }}</span>
+    </div>
+    <div class="flex">
+        <span class="w-64 font-semibold text-gray-700">Nama Lengkap</span>
+        <span class="text-gray-900">: {{ $aktaPerkawinan->perkawinanAyahIstri->nama_lengkap ?? '-' }}</span>
+    </div>
+    <div class="flex">
+        <span class="w-64 font-semibold text-gray-700">Tempat Tanggal Lahir</span>
+        <span class="text-gray-900">: {{ $aktaPerkawinan->perkawinanAyahIstri->tempat_lahir ?? '-' }},
+            {{ tglIndo($aktaPerkawinan->perkawinanAyahIstri->tanggal_lahir) }}</span>
+    </div>
+    <div class="flex">
+        <span class="w-64 font-semibold text-gray-700">Alamat</span>
+        <span class="text-gray-900">:
+            {{ $aktaPerkawinan->perkawinanAyahIstri->alamat }},
+            {{ $aktaPerkawinan->perkawinanAyahIstri->desa_kelurahan }},
+            RT {{ $aktaPerkawinan->perkawinanAyahIstri->rt }},
+            RW {{ $aktaPerkawinan->perkawinanAyahIstri->rw }},
+            {{ $aktaPerkawinan->perkawinanAyahIstri->kabupaten_kota }},
+            {{ $aktaPerkawinan->perkawinanAyahIstri->kecamatan }},
+            {{ $aktaPerkawinan->perkawinanAyahIstri->provinsi }},
+            {{ $aktaPerkawinan->perkawinanAyahIstri->kode_pos }}
+        </span>
+    </div>
+    <div class="flex">
+        <span class="w-64 font-semibold text-gray-700">Telepon</span>
+        <span class="text-gray-900">: {{ $aktaPerkawinan->perkawinanAyahIstri->telepon ?? '-' }}</span>
+    </div>
+    <div class="flex">
+        <span class="w-64 font-semibold text-gray-700">Pendidikan Terakhir</span>
+        <span class="text-gray-900">: {{ $aktaPerkawinan->perkawinanAyahIstri->pendidikan_terakhir ?? '-' }}</span>
+    </div>
+    <div class="flex">
+        <span class="w-64 font-semibold text-gray-700">Agama</span>
+        <span class="text-gray-900">: {{ $aktaPerkawinan->perkawinanAyahIstri->agama ?? '-' }}</span>
+    </div>
+    <div class="flex">
+        <span class="w-64 font-semibold text-gray-700">Nama Organisasi Penghayat</span>
+        <span class="text-gray-900">: {{ $aktaPerkawinan->perkawinanAyahIstri->organisasi_penghayat ?? '-' }}</span>
+    </div>
+</div>
